96345aa5af9b5d7e226c028303606f0a7f5ca616
Jakub Garstka <jakub.garstka97@gmail.com>
2020-05-08 17:40:05 +0200

    Options examples added

--- src/main/scala/Options/Options.scala
+++ src/main/scala/Options/Options.scala
@@ -2,13 +2,34 @@ package Options
 
 object Options extends App {
 
-// null:
+    // val x : Int = null
+    // val y : Boolean = null
 
-  //val x : Int = null
-  val y : Boolean = null
+    val list : List[String] = null
 
-  val list : List[String] = null
-  println(list)
-  //list.reverse
+    try { list.appended() } catch {
+      case e: NullPointerException => println("Odwołanie do nulla")
+    }
+
+  def toInt(s: String): Option[Int] = {
+    try {
+      Some(Integer.parseInt(s.trim))
+    } catch {
+      case e: Exception => None
+    }
+  }
+
+
+  //null "" "5"
+  val i = toInt(null)
+  println(i)
+  println(i.get)
+  println(i.getOrElse("10"))
+
+  
+  toInt(null) match {
+    case Some(x) => println(x)
+    case None => println("That didn't work.")
+  }
 
 }
