ae0966821bfda81821a81e566814cbf7ce213b28
Jakub Garstka <jakub.garstka97@gmail.com>
2020-05-10 14:01:26 +0200

    Exceptions added

--- src/main/scala/Exceptions/Exceptions.scala
+++ src/main/scala/Exceptions/Exceptions.scala
@@ -1,5 +1,34 @@
 package Exceptions
+import java.io.FileReader
+import java.io.FileNotFoundException
 
 object Exceptions {
+  // Object => Throwable => Exceptions
 
+  def main(args: Array[String]) {
+    try {
+      val f = new FileReader("file.txt")
+    } catch {
+      case ex: FileNotFoundException =>{
+        println("Missing file exception")
+      }
+    }
+      /*
+    finally {
+      println("Finally do this")
+    }
+
+       */
+/*
+    val e = new ExceptionExample()
+    try{
+      e.validate(5)
+    }catch{
+      case e : Exception => println("Exception Occured : "+e)
+    }
+
+ */
+
+  }
 }
+
---
+++ src/main/scala/Exceptions/InvalidAgeException.scala
@@ -0,0 +1,13 @@
+package Exceptions
+
+class InvalidAgeException(s:String) extends Exception(s){}
+class ExceptionExample{
+  @throws(classOf[InvalidAgeException])
+  def validate(age:Int){
+    if(age<18){
+      throw new InvalidAgeException("Not eligible")
+    }else{
+      println("You are eligible")
+    }
+  }
+}
